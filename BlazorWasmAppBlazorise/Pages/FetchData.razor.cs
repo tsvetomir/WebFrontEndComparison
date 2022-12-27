eb9d92c237ed507048ebcdf99bc0338eb1268c99
Mladen Macanović <st.srki@gmail.com>
2022-12-27 23:00:58 +0100

    Add Blazorise project (#1)

---
+++ BlazorWasmAppBlazorise/Pages/FetchData.razor.cs
@@ -0,0 +1,14 @@
+﻿using CSharpSharedData;
+
+namespace BlazorWasmAppBlazorise.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[]? forecasts;
+
+        protected override void OnInitialized()
+        {
+            forecasts = WeatherForecast.Data;
+        }
+    }
+}
